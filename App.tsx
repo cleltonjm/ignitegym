876962cafe96ceabe598bd51417486992d793608
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2022-12-20 22:03:06 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { Routes } from './src/routes';
+
 import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
-import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +18,7 @@ export default function App() {
         translucent
       />
       
-      {fontsLoaded ? <SignUp /> : <Loading />}      
+      {fontsLoaded ? <Routes /> : <Loading />}      
     </NativeBaseProvider>
   );
 }
