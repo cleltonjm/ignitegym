c3147560ab0c82b5807026022891e7fca7c0e9dc
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2022-12-22 21:43:12 -0300

    Add files via upload

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,45 @@
-import { Center, Text } from  'native-base';
+import { useState } from 'react';
+import { VStack, FlatList, HStack, Text, Heading } from 'native-base';
+
+import { HomeHeader } from '@components/HomeHeader';
+import { Group } from '@components/Group';
 
 export function Home() {
+    const [groups, setGroups] = useState(['costa', 'bíceps', 'tríceps', 'ombro' ]);
+    const [groupSelected, setGroupSelected] = useState('costa');
+
     return (
-        <Center flex={1}>
-            <Text>Home</Text>
-        </Center>
+        <VStack flex={1}>
+            <HomeHeader />
+
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group 
+                        name={item} 
+                        isActive={groupSelected === item} 
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxHeight={10}
+            />
+
+            <VStack flex={1} px={8}>
+                <HStack justifyContent="space-between" mb={5}>
+                    <Heading color="gray.200" fontSize="md">
+                        Exercícios
+                    </Heading>
+
+                    <Text color="gray.200" fontSize="sm">
+                        4
+                    </Text>
+                </HStack>
+            </VStack>
+        </VStack>
     );
 }
