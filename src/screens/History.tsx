e175c90a393a59ed94bfaaf6be869abf602e7462
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-01-31 22:26:54 -0300

    Add files via upload

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,39 @@
-import { Center, Text } from  'native-base';
+import { useState } from 'react';
+import { Heading, VStack, SectionList } from  'native-base';
+
+import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
+
 
 export function History() {
+    const [exercises, setExercise] = useState([
+        {
+            title: "31.01.2023",
+            data: ["Puxada frontal", "Remada unilateral"]
+        },
+        {
+            title: "05.02.2023",
+            data: ["Puxada frontal"]
+        },
+    ]);
+
     return (
-        <Center flex={1}>
-            <Text>History</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Histórico de Exercícios" />
+
+            <SectionList 
+                sections={exercises}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <HistoryCard />
+                )}
+                renderSectionHeader={({ section }) => (
+                    <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+                        {section.title}
+                    </Heading>
+                )}
+                px={8}
+            />
+        </VStack>
     );
 }
