c53d620d02c85b4371338faee680d9a52eb73ed2
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-03-15 19:31:51 -0300

    Add files via upload

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,12 +1,15 @@
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { Pattern } from 'react-native-svg';
 
 type FormDataProps = {
     name: string;
@@ -15,9 +18,18 @@ type FormDataProps = {
     password_confirm: string;
 }
 
+const signUpSchema = yup.object({
+    name: yup.string().required('Informe o nome.'),
+    email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
+    password: yup.string().required('Informe a senha.').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+    password_confirm: yup.string().required('Confirme a senha.').oneOf([yup.ref('password')], 'A confirmação da senha não confere.')
+});
+
 export function SignUp() {
 
-    const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
+    const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+        resolver: yupResolver(signUpSchema)
+    });
 
     const navigation = useNavigation();
 
@@ -56,21 +68,15 @@ export function SignUp() {
                 <Controller 
                     control={control}
                     name="name"
-                    rules={{
-                        required: 'Informe o nome.'
-                    }}
                     render={({ field: {onChange, value} }) => (
                         <Input 
                             placeholder="Nome"
                             onChangeText={onChange}
                             value={value}
+                            errorMessage={errors.name?.message}
                         />
                     )}
                 />
-
-                <Text color="white">
-                    {errors.name?.message}
-                </Text>
 
                 <Controller 
                     control={control}
@@ -82,6 +88,7 @@ export function SignUp() {
                             autoCapitalize="none"
                             onChangeText={onChange}
                             value={value}
+                            errorMessage={errors.email?.message}
                         />
                     )}
                 />
@@ -95,6 +102,7 @@ export function SignUp() {
                             secureTextEntry
                             onChangeText={onChange}
                             value={value}
+                            errorMessage={errors.password?.message}
                         />
                     )}
                 />
@@ -110,6 +118,7 @@ export function SignUp() {
                             value={value}
                             onSubmitEditing={handleSubmit(handleSignUp)}
                             returnKeyType="send"
+                            errorMessage={errors.password_confirm?.message}
                         />
                     )}
                 />
@@ -123,7 +132,7 @@ export function SignUp() {
             <Button 
                 title="Voltar para o login" 
                 variant="outline" 
-                mt={24} 
+                mt={12} 
                 onPress={handleGoBack}
             />   
         
