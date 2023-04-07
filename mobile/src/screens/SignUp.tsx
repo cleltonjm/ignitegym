4bb3ce31d7ff4f72d0976875c41b6b3e8f4f3446
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-04-06 22:09:56 -0300

    Add files via upload

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -4,12 +4,16 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
+import axios from 'axios';
+import { api } from '@services/api';
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { Pattern } from 'react-native-svg';
+import { Alert } from 'react-native';
 
 type FormDataProps = {
     name: string;
@@ -38,16 +42,14 @@ export function SignUp() {
     }
 
     async function handleSignUp({ name, email, password, password_confirm }: FormDataProps){
-        const response = await fetch('http://192.168.0.104:3333/users', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ name, email, password })
-        });
-            const data = await response.json();
-            console.log(data);
+        try {
+            const response = await api.post('/users', { name, email, password });
+            console.log(response.data); 
+        } catch(error) {
+            if(axios.isAxiosError(error)){
+                Alert.alert(error.response?.data.message);    
+            } 
+        }        
     }
 
     return(
